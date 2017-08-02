6ee5e7be786fc3d768ffa4d7efe3060812985f61
Cole Kissane <coler706@gmail.com>
2017-08-02 08:23:56 -0700

    More Car Stuff

--- car.js
+++ car.js
@@ -2,9 +2,18 @@ function Car(data){
     if(data){
         this.data=data;
     }else{
-        this.data={carBody:{lengths:[50,50,50,50,50,50,50,50],angleSize:[45,45,45,45,45,45,45,45]},wheels:[]};
-        for(var i=0;i<8;i++){
-            this.data.wheels.push({index:i,radius:10});
+        this.data={carBody:{lengths:[],angleWeight:[]},wheels:[]};
+        for(var i=0;i<this.bodyParts;i++){
+            this.data.carBody.lengths.push(this.maxLength/2);
+            this.data.carBody.angleWeight.push(0.5);
+            this.data.wheels.push({index:i,radius:this.maxRadius/2});
         }
+        
     }
+}
+Car.prototype.bodyParts=12;
+Car.prototype.maxLength=100;
+Car.prototype.maxRadius=50;
+Car.prototype.breed=function(other){
+    
 }
