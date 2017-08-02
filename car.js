31e7b92f656500a916c81ebff3ec671a35b5b190
Cole Kissane <coler706@gmail.com>
2017-08-02 08:17:49 -0700

    Car Base

--- car.js
+++ car.js
@@ -2,6 +2,9 @@ function Car(data){
     if(data){
         this.data=data;
     }else{
-        this.data={carBody:[50,50,50,50,50,50,50,50],wheels:[{index:1,radius:10}]};
+        this.data={carBody:{lengths:[50,50,50,50,50,50,50,50],angleSize:[45,45,45,45,45,45,45,45]},wheels:[]};
+        for(var i=0;i<8;i++){
+            this.data.wheels.push({index:i,radius:10});
+        }
     }
 }
