8a2f3db4b546e95dfa809e6851c09c0b42507b19
modderme123 <modderme123@gmail.com>
2017-08-02 08:29:34 -0700

    format

--- car.js
+++ car.js
@@ -1,19 +1,17 @@
-function Car(data){
-    if(data){
-        this.data=data;
-    }else{
-        this.data={carBody:{lengths:[],angleWeight:[]},wheels:[]};
-        for(var i=0;i<this.bodyParts;i++){
-            this.data.carBody.lengths.push(this.maxLength/2);
+function Car(data) {
+    if (data) {
+        this.data = data;
+    } else {
+        this.data = { carBody: { lengths: [], angleWeight: [] }, wheels: [] };
+        for (var i = 0; i < this.bodyParts; i++) {
+            this.data.carBody.lengths.push(this.maxLength / 2);
             this.data.carBody.angleWeight.push(0.5);
-            this.data.wheels.push({index:i,radius:this.maxRadius/2});
+            this.data.wheels.push({ index: i, radius: this.maxRadius / 2 });
         }
-        
+
     }
 }
-Car.prototype.bodyParts=12;
-Car.prototype.maxLength=100;
-Car.prototype.maxRadius=50;
-Car.prototype.breed=function(other){
-    
-}
+Car.prototype.bodyParts = 12;
+Car.prototype.maxLength = 100;
+Car.prototype.maxRadius = 50;
+Car.prototype.breed = function (other) {}
